e5935f1ba68a00f89cf09badd4acbbf6d536ff01
colemccall <colemccall@nnu.edu>
2022-12-13 14:45:13 -0700

    reorganized notebooks

--- GE_Outcomes_V2/Code/OpenPredictionNotebook.bat
+++ GE_Outcomes_V2/Code/OpenPredictionNotebook.bat
@@ -1,3 +1,3 @@
-set project=C:\Users\colem\Desktop\Senior\BigData\GE_BigData_Project\GE_Outcomes_V2\Notebooks\Statistics_and_ML.ipynb
+set project=C:\Users\colem\Desktop\Senior\BigData\GE_BigData_Project\GE_Outcomes_V2\Notebooks\MachineLearning\Statistics_and_ML.ipynb
 
 code %project% 
